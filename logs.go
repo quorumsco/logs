971ae4bb7a41388bd73d1770b14d3c1a3847570e
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-03 10:38:27 +0200

    Add customizable log level

--- logs.go
+++ logs.go
@@ -8,34 +8,97 @@ import (
 	"github.com/fatih/color"
 )
 
-func logger(level string, c color.Attribute) func(format interface{}, v ...interface{}) {
-	colorPrint := color.New(c).SprintFunc()
-	return func(format interface{}, v ...interface{}) {
-		switch format.(type) {
-		case string:
-			log.Printf("%s %s\n", colorPrint(level), fmt.Sprintf(format.(string), v...))
-		case error:
-			log.Printf("%s %s\n", colorPrint(level), fmt.Sprint(format.(error)))
-		default:
-			log.Printf("%s %v\n", colorPrint(level), format)
-		}
+// DefaultLogger provide a simple interface et sane defaults.
+// By default, it will not log debug infos.
+var DefaultLogger = &Logger{Level: 4}
+
+const (
+	CriticalLevel = iota
+	ErrorLevel
+	WarningLevel
+	NoticeLevel
+	InfoLevel
+	DebugLevel
+)
+
+type LevelInfo struct {
+	Str   string
+	Color color.Attribute
+}
+
+var Levels = map[int]LevelInfo{
+	CriticalLevel: {Str: "CRIT", Color: color.FgMagenta},
+	ErrorLevel:    {Str: "ERRO", Color: color.FgRed},
+	WarningLevel:  {Str: "WARN", Color: color.FgYellow},
+	NoticeLevel:   {Str: "NOTI", Color: color.FgGreen},
+	InfoLevel:     {Str: "INFO", Color: color.Reset},
+	DebugLevel:    {Str: "DEBU", Color: color.FgCyan},
+}
+
+// Logger limit the output logs to a specified level.
+type Logger struct {
+	Level int
+}
+
+func logs(level int, maxLevel int, format interface{}, v ...interface{}) {
+	if level > maxLevel {
+		return
+	}
+	levelStr := Levels[level].Str
+	colorPrint := color.New(Levels[level].Color).SprintFunc()
+	switch format.(type) {
+	case string:
+		log.Printf("%s %s\n", colorPrint(levelStr), fmt.Sprintf(format.(string), v...))
+	case error:
+		log.Printf("%s %s\n", colorPrint(levelStr), fmt.Sprint(format.(error)))
+	default:
+		log.Printf("%s %v\n", colorPrint(levelStr), format)
 	}
 }
 
+func (l Logger) Debug(format interface{}, v ...interface{}) {
+	logs(DebugLevel, l.Level, format, v...)
+}
+
+func (l Logger) Info(format interface{}, v ...interface{}) {
+	logs(InfoLevel, l.Level, format, v...)
+}
+
+func (l Logger) Notice(format interface{}, v ...interface{}) {
+	logs(NoticeLevel, l.Level, format, v...)
+}
+
+func (l Logger) Warning(format interface{}, v ...interface{}) {
+	logs(WarningLevel, l.Level, format, v...)
+}
+
+func (l Logger) Error(format interface{}, v ...interface{}) {
+	logs(ErrorLevel, l.Level, format, v...)
+}
+
+func (l Logger) Critical(format interface{}, v ...interface{}) {
+	logs(CriticalLevel, l.Level, format, v...)
+}
+
+// Level set the DefaultLogger log level
+func Level(level int) {
+	DefaultLogger.Level = level
+}
+
 // Debug logger.
-var Debug = logger("DEBU", color.FgCyan)
+var Debug = DefaultLogger.Debug
 
 // Info logger.
-var Info = logger("INFO", color.Reset)
+var Info = DefaultLogger.Info
 
 // Notice logger.
-var Notice = logger("NOTI", color.FgGreen)
+var Notice = DefaultLogger.Notice
 
 // Warning logger.
-var Warning = logger("WARN", color.FgYellow)
+var Warning = DefaultLogger.Warning
 
 // Error logger.
-var Error = logger("ERRO", color.FgRed)
+var Error = DefaultLogger.Error
 
 // Critical logger.
-var Critical = logger("CRIT", color.FgMagenta)
+var Critical = DefaultLogger.Critical
